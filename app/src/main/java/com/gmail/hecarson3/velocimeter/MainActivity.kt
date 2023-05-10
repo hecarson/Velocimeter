978c9b00d27631db61765cd49f1fd9c6a2f84fad
TheWildDefender <hecarson3@gmail.com>
2023-05-10 02:29:48 -0600

    removed navigation and put settings page in new activity

--- app/src/main/java/com/gmail/hecarson3/velocimeter/MainActivity.kt
+++ app/src/main/java/com/gmail/hecarson3/velocimeter/MainActivity.kt
@@ -1,28 +1,19 @@
 package com.gmail.hecarson3.velocimeter
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Button
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.NavHostFragment
 
 class MainActivity : AppCompatActivity(), VelocimeterFragment.SettingsNavigator {
-
-    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-//        navController = findNavController(R.id.fragmentContainerView)
-        navController = navHostFragment.navController
     }
 
     override fun onSettingsNavigate() {
-        navController.navigate(R.id.action_velocimeterFragment_to_settingsFragment)
+        val settingsIntent = Intent(this, SettingsActivity::class.java)
+        startActivity(settingsIntent)
     }
 
 }
